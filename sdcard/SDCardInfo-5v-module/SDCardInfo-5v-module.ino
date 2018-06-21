7c08ab60bc444762bce002550f9c8093044a88db
jacob-jarick <mem.namefix@gmail.com>
2018-06-22 01:16:18 +0800

    comments updated

--- sdcard/SDCardInfo-5v-module/SDCardInfo-5v-module.ino
+++ sdcard/SDCardInfo-5v-module/SDCardInfo-5v-module.ino
@@ -1,5 +1,6 @@
 /*
-  SD card test
+  SD card test - updated for STM32 bluepill. for use with the common 5v module.
+  ref: http://www.stm32duino.com/viewtopic.php?f=2&t=3234&hilit=sdfat&sid=a0e040280616f5e6e9c033029b703c52&start=10#p41771
 
   This example shows how use the utility libraries on which the'
   SD library is based in order to get info about your SD card.
@@ -7,17 +8,21 @@
 
   The circuit:
     SD card attached to SPI bus as follows:
- ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
- ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
- ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
- ** CS - depends on your SD card shield or module.
- 		Pin 4 used here for consistency with other Arduino examples
+ ** MOSI - PB14
+ ** MISO - PB15
+ ** CLK - PB13
+ ** CS - PB12
+ ** VCC - 5V
+ ** GND - GND
 
 
   created  28 Mar 2011
   by Limor Fried
   modified 9 Apr 2012
   by Tom Igoe
+  modified 22/06/2018
+  by Jacob Jarick
+  
 */
 // include the SD library:
 #include <SPI.h>
@@ -33,12 +38,13 @@ SdFile root;
 // Adafruit SD shields and modules: pin 10
 // Sparkfun SD shield: pin 8
 // MKRZero SD: SDCARD_SS_PIN
+// Bluepill: PB12
 const int chipSelect = PB12;
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  delay(10000);
+  delay(10000); // wait for win10 to init usb serial
 
   Serial.print("\nInitializing SD card...");
   SPI.setModule (2);  // SPI port 2 (5v port)
